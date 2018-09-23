6eb5bf869535f99d221f63e209fcbbe82349aa1d
Boguslaw Mista <bodziomista@gmail.com>
2018-09-23 23:26:53 +0200

    Betta git history printing

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 alias gpull='git pull --prune'
 alias gl="git log -n 20 --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias glp="git log -n 20 --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glp="git log -n 20 --graph --decorate --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gpush='git push origin HEAD'
 alias ga='git add'
 alias gap='git add -p'
@@ -9,6 +9,10 @@ alias gdc='git diff --cached'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gco='git checkout'
-alias gb='git branch'
+alias gb='git branch -v'
 alias gs='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gbr="git branch -r"
+
+alias gl1="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias gl2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
